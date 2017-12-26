feaf4bebc0f5def9d9f58b9064ad79902e70e5ce
givemin5 <givemin5@gmail.com>
2017-12-26 15:42:04 +0800

    加入專案檔案。

---
+++ ZipFileDemo/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZipFileDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sourcePath = @"D:\SourceFiles";
+            string targetPath = @"D:\Backup";
+
+            ZipProcess zipProcess = new ZipProcess(sourcePath, targetPath);
+
+            zipProcess.Do();
+
+        }
+    }
+}
---
+++ ZipFileDemo/ZipProcess.cs
@@ -0,0 +1,44 @@
+﻿using Ionic.Zip;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZipFileDemo
+{
+    class ZipProcess
+    {
+        private string sourcePath;
+        private string targetPath;
+
+        public ZipProcess(string sourcePath, string targetPath)
+        {
+            this.sourcePath = sourcePath;
+            this.targetPath = targetPath;
+        }
+
+        public void Do()
+        {
+            string date = DateTime.Now.ToString("yyyyMMdd");
+            string zipFileName = $"Backup_{date}.zip";
+
+            string fullZipPath = $"{targetPath}\\{zipFileName}";
+
+            FileInfo file = new FileInfo(fullZipPath);
+
+            using (ZipFile zip = new ZipFile())
+            {
+                zip.AlternateEncodingUsage = ZipOption.Default;
+                zip.AddDirectory(sourcePath);
+                zip.Comment = "This zip was created at " + System.DateTime.Now.ToString("G");
+
+                if (file.Exists)
+                    file.Delete();
+
+                zip.Save(fullZipPath);
+            }
+        }
+    }
+}
